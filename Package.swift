1f6cdb9167b585e6b2605bb6cec098b0cf083222
Szymon Gniado <8l3ndz10r@protonmail.com>
2025-02-27 22:01:43 +0100

    added docs directory

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
   name: "ant-algorithm",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+  ],
   targets: [
     .executableTarget(
       name: "AntAlgorithm")
